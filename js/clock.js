d73a732ab524096268b307deca19420bf32db43d
Sajjad Mirza <smirza@calpoly.edu>
2015-01-25 14:48:32 -0800

    Weather clock (lab 4)

--- js/clock.js
+++ js/clock.js
@@ -3,4 +3,41 @@ function getTime() {
    setTimeout(getTime, 1000);
 }
 
-window.onload = getTime;
+function getTemp() {
+   $.getJSON(
+      "https://api.forecast.io/forecast/abb5355d28533ac9d3091d7e872f8347/35.300399,-120.662362?callback=?",
+      {},
+      function(data) {
+         var maxtemp;
+         var cls;
+         
+         $("#forecastLabel").html(data.daily.summary);
+         $("#forecastIcon").attr("src", "img/"+data.daily.icon+".png");
+         maxtemp = data.daily.data[0].temperatureMax;
+         console.log(maxtemp);
+         cls;
+         if (maxtemp >= 90) {
+            cls = "hot";
+         }
+         else if (maxtemp >= 80) {
+            cls = "warm";
+         }
+         else if (maxtemp >= 70) {
+            cls = "nice";
+         }
+         else if (maxtemp >= 60) {
+            cls = "chilly";
+         }
+         else {
+            cls = "cold";
+         }
+
+         $("body").addClass(cls);
+      }
+   );
+}
+
+window.onload = function() {
+   getTime();
+   getTemp();
+};
